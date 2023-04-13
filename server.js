364aa0093e7e28f67f62e3f4fe7f2a2fd45da12a
Chrixco <christianxcorral@gmail.com>
2023-04-13 22:36:49 +0200

    up

--- server.js
+++ server.js
@@ -2,12 +2,22 @@ const express = require('express');
 const multer = require('multer');
 const app = express();
 const upload = multer({ dest: 'uploads/' });
+const fileType = require('file-type');
+const fs = require('fs');
 
-app.post('/upload', upload.single('file'), (req, res) => {
+app.post('/upload', upload.single('file'), async (req, res) => {
   if (!req.file) {
     res.status(400).send('No file uploaded.');
     return;
   }
+
+  const fileBuffer = fs.readFileSync(req.file.path);
+  const type = await fileType.fromBuffer(fileBuffer);
+  if (type.ext !== 'pdf' && type.ext !== 'txt') {
+    return res.status(400).json({ error: 'Invalid file type. Only PDF and text files are allowed.' });
+  }
+
+  // Process the uploaded file
   res.send('File uploaded successfully.');
 });
 
